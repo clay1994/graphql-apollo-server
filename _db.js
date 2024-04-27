1c660f9cbefda5cdf63c28f3eff15c5a46e6764a
clay1994 <rupasinghekamila@gmail.com>
2024-04-27 19:01:29 +1000

    add query variables

--- _db.js
+++ _db.js
@@ -13,7 +13,7 @@ let games = [
   ]
   
   let reviews = [
-    {id: '1', rating: 9, content: 'lorem ipsum', author_id: '1', game_id: '2'},
+    {id: '1', rating: 9, content: 'lorem ipsum_first', author_id: '1', game_id: '2'},
     {id: '2', rating: 10, content: 'lorem ipsum', author_id: '2', game_id: '1'},
     {id: '3', rating: 7, content: 'lorem ipsum', author_id: '3', game_id: '3'},
     {id: '4', rating: 5, content: 'lorem ipsum', author_id: '2', game_id: '4'},
