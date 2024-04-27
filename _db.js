a39878050d6c2e3ed4909a329cd2afec1b6d26fb
clay1994 <rupasinghekamila@gmail.com>
2024-04-27 21:15:50 +1000

    reladed date between types

--- _db.js
+++ _db.js
@@ -14,12 +14,12 @@ let games = [
   
   let reviews = [
     {id: '1', rating: 9, content: 'lorem ipsum_first', author_id: '1', game_id: '2'},
-    {id: '2', rating: 10, content: 'lorem ipsum', author_id: '2', game_id: '1'},
-    {id: '3', rating: 7, content: 'lorem ipsum', author_id: '3', game_id: '3'},
-    {id: '4', rating: 5, content: 'lorem ipsum', author_id: '2', game_id: '4'},
-    {id: '5', rating: 8, content: 'lorem ipsum', author_id: '2', game_id: '5'},
-    {id: '6', rating: 7, content: 'lorem ipsum', author_id: '1', game_id: '2'},
-    {id: '7', rating: 10, content: 'lorem ipsum', author_id: '3', game_id: '1'},
+    {id: '2', rating: 10, content: 'lorem ipsum3', author_id: '2', game_id: '1'},
+    {id: '3', rating: 7, content: 'lorem ipsum3', author_id: '3', game_id: '3'},
+    {id: '4', rating: 5, content: 'lorem ipsum4', author_id: '2', game_id: '4'},
+    {id: '5', rating: 8, content: 'lorem ipsum5', author_id: '2', game_id: '5'},
+    {id: '6', rating: 7, content: 'lorem ipsum6', author_id: '1', game_id: '2'},
+    {id: '7', rating: 10, content: 'lorem ipsum7', author_id: '3', game_id: '1'},
   ]
   
   export default { games, authors, reviews }
--- schema.js
+++ schema.js
@@ -3,22 +3,26 @@ export const typeDefs = `#graphql
     id: ID!
     title: String!
     platform: [String!]!
+    reviews: [Review!]
   }
   type Review {
     id: ID!
     rating: Int!
     content: String!
+    author: Author!
+    game: Game!
   }
   type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
   }
   type Query {
-    reviews: [Review]
-    review(id: ID): Review
     games: [Game]
     game(id: ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
     authors: [Author]
     author(id: ID!): Author
   }
